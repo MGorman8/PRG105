fa87287f4ba70e5751f14630d96b1ceea81c6f52
MGorman8 <31226611+MGorman8@users.noreply.github.com>
2017-09-15 21:58:04 -0500

    Add files via upload

---
+++ If_Else.py
@@ -0,0 +1,25 @@
+# Variable
+number = int(input("What number would you like to view as a Roman Numeral? 1 - 10 :"))
+
+if number == 1:
+    print("The Roman Numeral for 1 is I.")
+elif number == 2:
+    print("The Roman Numeral for 2 is II.")
+elif number == 3:
+    print("The Roman Numeral for 3 is III.")
+elif number == 4:
+    print("The Roman Numeral for 4 is IV.")
+elif number == 5:
+    print("The Roman Numeral for 5 is V.")
+elif number == 6:
+    print("The Roman Numeral for 6 is VI.")
+elif number == 7:
+    print("The Roman Numeral for 7 is VII.")
+elif number == 8:
+    print("The Roman Numeral for 8 is VIII.")
+elif number == 9:
+    print("The Roman Numeral for 9 is IX.")
+elif number == 10:
+    print("The Roman Numeral for 10 is X.")
+else:
+    print("You did not choose a number between 1 and 10!")
